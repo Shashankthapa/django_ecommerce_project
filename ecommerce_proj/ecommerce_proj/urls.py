93a448a0696209e1c9dbe15470415c25f6f00bf7
shashankthapa <shashankthapa022@gmail.com>
2022-12-06 08:13:38 +0545

    update

--- ecommerce_proj/ecommerce_proj/urls.py
+++ ecommerce_proj/ecommerce_proj/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -5,3 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ecom_app.urls'))
 ]
+
+if settings.DEBUG:
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
